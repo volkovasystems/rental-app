fb9a093692ef94aa37feb5720e6b06f8b05fd8d4
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2015-07-24 16:48:13 +0800

    Fix directories.

--- tool/start-alpha.sh
+++ tool/start-alpha.sh
@@ -14,8 +14,8 @@ fi
 
 mv *.log *.err *.out ~/.forever/*.log "../logs/$(echo `date +%Y-%m-%d`)";
 
-forever start -l alphadb.log -o alphadb.out -e alphadb.err -m 3 server/database-main.js --option=../local/alpha-database.js;
+forever start -l alphadb.log -o alphadb.out -e alphadb.err -m 3 server/database/database-main.js --option=../../local/alpha-database.js;
 
-forever start -l alpha.log -o alpha.out -e alpha.err -m 3 server/alpha-main.js --option=../local/alpha-server.js;
+forever start -l alpha.log -o alpha.out -e alpha.err -m 3 server/alpha/alpha-main.js --option=../../local/alpha-server.js;
 
-forever start -l static.log -o static.out -e static.err -m 3 server/static-main.js --option=../local/static-server.js;
+forever start -l static.log -o static.out -e static.err -m 3 server/static/static-main.js --option=../../local/static-server.js;
